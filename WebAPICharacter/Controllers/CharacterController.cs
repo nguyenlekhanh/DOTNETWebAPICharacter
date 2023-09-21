920cee42224ea7618460f74dcb9c506140191edf
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-20 21:23:04 -0500

    Move logic from Controller to Services file and add service to the builder services addScope

--- WebAPICharacter/Controllers/CharacterController.cs
+++ WebAPICharacter/Controllers/CharacterController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
 namespace WebAPICharacter.Controllers
 {
     [ApiController]
@@ -12,23 +13,28 @@ namespace WebAPICharacter.Controllers
             new Character { Id = 1, Name = "Sam" }
         };
 
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService characterService) 
+        {
+            _characterService = characterService;
+        }
+
         [HttpGet("GetAll")]
         public ActionResult<List<Character>> Get()
         {
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet("GetSingle")]
         public ActionResult<Character> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c=>c.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost("AddCharacter")]
         public ActionResult<List<Character>> AddCharacter(Character newCharacter)
         {
-            characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
---
+++ WebAPICharacter/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPICharacter.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
