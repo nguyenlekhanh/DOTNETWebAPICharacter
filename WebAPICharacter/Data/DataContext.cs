0843c69d93e7b38aad9d287157646609f7398898
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-21 22:48:08 -0500

    add entifyFrameworkCore add connectionString add DbContext

---
+++ WebAPICharacter/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace WebAPICharacter.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Character> Character => Set<Character> ();
+    }
+}
--- WebAPICharacter/Program.cs
+++ WebAPICharacter/Program.cs
@@ -1,12 +1,22 @@
 global using WebAPICharacter.Models;
 global using WebAPICharacter.Services.CharacterService;
 global using WebAPICharacter.Dtos.Character;
+global using Microsoft.EntityFrameworkCore;
+global using WebAPICharacter.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
