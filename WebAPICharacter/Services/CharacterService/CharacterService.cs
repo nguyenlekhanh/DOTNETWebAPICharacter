62da5026bfaff021953f210c8b52d7ef5b89907d
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-21 23:17:53 -0500

    Implement CRUD to DbContext

--- WebAPICharacter/Services/CharacterService/CharacterService.cs
+++ WebAPICharacter/Services/CharacterService/CharacterService.cs
@@ -10,18 +10,27 @@ namespace WebAPICharacter.Services.CharacterService
             new Character(),
             new Character { Id = 1, Name = "Sam" }
         };
+        private readonly DataContext _context;
         private readonly IMapper _mapper;
 
-        public CharacterService(IMapper mapper) 
-        { 
+        public CharacterService(IMapper mapper, DataContext context) 
+        {
+            _context = context;
             _mapper = mapper;
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             var serviceRepsonse = new ServiceResponse<List<GetCharacterDto>>();
-            characters.Add(_mapper.Map<Character>(newCharacter));
-            serviceRepsonse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+
+            var addAsync = await _context.Character.AddAsync(_mapper.Map<Character>(newCharacter));
+            _context.SaveChanges();
+
+            var dbCharacters = await _context.Character.ToListAsync();
+            serviceRepsonse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            
+            //characters.Add(_mapper.Map<Character>(newCharacter));
+            //serviceRepsonse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceRepsonse;
         }
 
@@ -31,39 +40,43 @@ namespace WebAPICharacter.Services.CharacterService
 
             try
             {
-                var character = characters.FirstOrDefault(c => c.Id == id);
+                //var character = characters.FirstOrDefault(c => c.Id == id);
+                _context.Remove(_context.Character.Single(c => c.Id == id));
+                _context.SaveChanges();
 
-                if (character is null)
-                {
-                    throw new Exception($"Character with Id '{id}' not found");
-                }
+                //if (character is null)
+                //{
+                //    throw new Exception($"Character with Id '{id}' not found");
+                //}
 
-                characters.Remove(character);
+                //characters.Remove(character);
 
-                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
-
+                //serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+                var dbCharacters = await _context.Character.ToListAsync();
+                serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             }
+
             catch (Exception ex)
             {
                 serviceResponse.Success = false;
                 serviceResponse.Message = ex.Message;
             }
             return serviceResponse;
-            throw new NotImplementedException();
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceRepsonse = new ServiceResponse<List<GetCharacterDto>>();
-            serviceRepsonse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            var dbCharacters = await _context.Character.ToListAsync();
+            serviceRepsonse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceRepsonse;
         }
 
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceRepsonse = new ServiceResponse<GetCharacterDto>();
-            
-            var character = characters.FirstOrDefault(c => c.Id == id);
+
+            var character = await _context.Character.FirstOrDefaultAsync(c => c.Id == id);
 
             serviceRepsonse.Data = _mapper.Map<GetCharacterDto>(character);
             return serviceRepsonse;
@@ -75,7 +88,7 @@ namespace WebAPICharacter.Services.CharacterService
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
 
             try { 
-                var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+                var character = await _context.Character.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
 
                 if(character is null)
                 {
@@ -88,7 +101,7 @@ namespace WebAPICharacter.Services.CharacterService
                 character.Defense = updatedCharacter.Defense;
                 character.Intelligence = updatedCharacter.Intelligence;
                 character.Class = updatedCharacter.Class;
-
+                _context.SaveChanges();
                 serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
 
             } catch (Exception ex)
